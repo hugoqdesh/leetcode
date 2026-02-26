8d3fe65b0cf66dc3045b6ddf890f50f10e6c68b7
hugoqdesh <gitjohan5@gmail.com>
2026-02-26 17:58:35 +0200

    Remove Element

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 void main() {
     int[] nums = {1,1,2};
+    int val = 3;
 
-    System.out.println(new RemoveDuplicatesFromSortedArray().removeDuplicates(nums));
+    System.out.println(new RemoveElement().removeElement(nums, val));
 }
